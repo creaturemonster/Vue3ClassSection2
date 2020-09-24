05d451d3a234f6615bae03ebccf9d6bf7caa26fe
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-24 17:11:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app=Vue.createApp({
   data(){
     return {
-      courseGoal:'Finish the course and learn Vue!', 
+      courseGoalA:'Finish the course and learn Vue!', 
+      courseGoalB:'<h2>Master Vue and build amazing apps!</h2>',
       vueLink: 'https://vuejs.org/';
     };
+  }, 
+  methods:{
+    outputGoal(){
+      const randomNumber=Math.random();
+      if(randomNumber < 0.5){
+        return 'Learn VUE!';
+      }else{
+        return 'Master VUE!';
+      }
+    }
   };
 });
 app.mount('#user-goal');
